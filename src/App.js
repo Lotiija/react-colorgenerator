255ee2ff28e26b2f2ef349292b00bb7745db46a2
Lotiija <milchyloti@gmial.com>
2024-03-11 15:36:47 +0100

    color generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [typeOfColor, setTypeOfColor] = useState("hex");
+  const [color, setColor] = useState("#000000");
+
+  const  handleCreateRandomRgbColor = () => {
+    const r = randomColorUtility(256);
+    const g = randomColorUtility(256);
+    const b = randomColorUtility(256);
+
+    setColor(`rgb(${r},${g},${b})`);
+
+  };
+
+  const randomColorUtility = (length) => {
+    return Math.floor(Math.random()*length)
+  }
+
+  const handleCreateRandomHexColor = () => {
+    const hex = [1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+    let hexColor = "#"
+
+    for(let i=0; i<6; i++){
+      hexColor += hex[randomColorUtility(hex.length)]
+    }
+
+    setColor(hexColor);
+  };
+
+  useEffect(() => {
+    if (typeOfColor === "rgb") handleCreateRandomRgbColor();
+    else handleCreateRandomHexColor();
+  }, [typeOfColor, ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        width: "100vh",
+        height: "100vh",
+        background: color,
+      }}
+    >
+      <button onClick={() => setTypeOfColor("hex")}>Create HEX Color</button>
+      <button onClick={() => setTypeOfColor("rgb")}>Create RGB Color</button>
+      <button
+        onClick={
+          typeOfColor === "hex"
+            ? handleCreateRandomHexColor
+            : handleCreateRandomRgbColor
+        }
+      >
+        Generate Random Color
+      </button>
+      <div style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "#fff",
+        fontSize: "60px",
+        marginTop: "50px",
+        flexDirection: "column",
+        gap: "20px",
+      }}>
+        <h3>{typeOfColor === "rgb" ? "RGB Color" : "HEX Color"}</h3>
+        <h1>{color}</h1>
+      </div>
     </div>
   );
 }
